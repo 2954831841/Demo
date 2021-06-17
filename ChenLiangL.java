5576dae5466d9608312f10cc33e7bb422333df03
3149074438 <85997458+3149074438@users.noreply.github.com>
2021-06-17 19:15:47 +0800

    陈亮
    主程序

---
+++ ChenLiangL.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ChenLiangL{
+	punlic static void main(String[] args){
+		Dome A=new Dome();
+		Scanner sc=new Scanner(System.in);
+		double a=sc.nextDouble();
+		double b=sc.nextDouble();
+		double c=sc.nextDouble();
+		if(c==1){A.arr(a,b);}
+		if(c==2){A.agg(a,b);}
+		
+	
+	}
+	
+}
